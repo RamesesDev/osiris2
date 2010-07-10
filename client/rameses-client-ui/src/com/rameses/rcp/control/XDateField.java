5b29cd750852825db7fb27a0ad831348c815b155
rameses <rameses@rameses-desktop.(none)>
2010-07-10 11:28:48 +0800

    added XTextArea, XDateField, XPasswordField, XNumberField

---
+++ client/rameses-client-ui/src/com/rameses/rcp/control/XDateField.java
@@ -0,0 +1,204 @@
+package com.rameses.rcp.control;
+
+import com.rameses.rcp.framework.Binding;
+import com.rameses.rcp.ui.Containable;
+import com.rameses.rcp.ui.ControlProperty;
+import com.rameses.rcp.ui.UIControl;
+import com.rameses.rcp.ui.UIInput;
+import com.rameses.rcp.ui.Validatable;
+import com.rameses.rcp.util.ActionMessage;
+import com.rameses.rcp.util.UIControlUtil;
+import com.rameses.rcp.util.UIInputUtil;
+import com.rameses.util.ValueUtil;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.beans.Beans;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JFormattedTextField;
+
+/**
+ *
+ * @author Windhel
+ */
+
+public class XDateField extends JFormattedTextField implements UIInput, Validatable, Containable {
+    
+    private boolean nullWhenEmpty = true;
+    private String[] depends;
+    private int index;
+    private Binding binding;
+    private Date currentDate;
+    private SimpleDateFormat inputFormatter;
+    private SimpleDateFormat outputFormatter;
+    private String outputFormat;
+    private String inputFormat;
+    private ControlProperty controlProperty = new ControlProperty();
+    private ActionMessage actionMessage = new ActionMessage();
+    
+    public XDateField() {
+        setOutputFormat("yyyy-MM-dd");
+        setInputFormat("yyyy-MM-dd");
+    }
+    
+    public Object getValue() {
+        if( Beans.isDesignTime())
+            return "";
+        
+        return getFormattedDate();
+    }
+    
+    public void setValue(Object value) {
+        value = outputFormatter.format(value);
+        setText( value==null? "" : value.toString() );
+    }
+    
+    public boolean isNullWhenEmpty() {
+        return nullWhenEmpty;
+    }
+    
+    public void setNullWhenEmpty(boolean nullWhenEmpty) {
+        this.nullWhenEmpty = nullWhenEmpty;
+    }
+    
+    public String[] getDepends() {
+        return depends;
+    }
+    
+    public void setDepends(String[] depends) {
+        this.depends = depends;
+    }
+    
+    public int getIndex() {
+        return index;
+    }
+    
+    public void setIndex(int index) {
+        this.index = index;
+    }
+    
+    public void setBinding(Binding binding) {
+        this.binding = binding;
+    }
+    
+    public Binding getBinding() {
+        return binding;
+    }
+    
+    public void refresh() {
+        Object value = UIControlUtil.getBeanValue(this);
+        setValue(value);
+    }
+    
+    public void load() {
+        setInputVerifier(UIInputUtil.VERIFIER);
+        
+        addFocusListener(new DateFieldSupport());
+    }
+    
+    public int compareTo(Object o) {
+        if( o == null || !(o instanceof UIControl) )
+            return 0;
+        
+        UIControl u = (UIControl)o;
+        return this.index - u.getIndex();
+    }
+    
+    private Date getFormattedDate() {
+        if( ValueUtil.isEmpty(getText()) )
+            return null;
+        
+        try {
+            return inputFormatter.parse(getText());
+        } catch(Exception e) {
+            throw new IllegalStateException("Expected format: " + inputFormat, e);
+        }
+    }
+    
+    public String getOutputFormat() {
+        return outputFormat;
+    }
+    
+    public void setOutputFormat(String pattern) {
+        this.outputFormat = pattern;
+        if( !ValueUtil.isEmpty(pattern) ) {
+            outputFormatter = new SimpleDateFormat(pattern);
+        } else {
+            outputFormatter = null;
+        }
+    }
+    
+    public String getInputFormat() {
+        return inputFormat;
+    }
+    
+    public void setInputFormat(String inputFormat) {
+        this.inputFormat = inputFormat;
+        if( !ValueUtil.isEmpty(inputFormat) ) {
+            inputFormatter = new SimpleDateFormat(inputFormat);
+        } else {
+            inputFormatter = null;
+        }
+    }
+    
+    private final void showFormattedValue(boolean formatted) {
+        Object value = UIControlUtil.getBeanValue(this);
+        if( formatted && outputFormatter !=null && value!=null ) {
+            setText( outputFormatter.format(value));
+        } else {
+            if( value == null )
+                setText("");
+            else
+                setText( inputFormatter.format(value).toString() );
+        }
+    }
+
+    public String getCaption() {
+        return controlProperty.getCaption();
+    }
+
+    public void setCaption(String caption) {
+        controlProperty.setCaption(caption);
+    }
+
+    public boolean isRequired() {
+        return controlProperty.isRequired();
+    }
+
+    public void setRequired(boolean required) {
+        controlProperty.setRequired(required);
+    }
+
+    public void validateInput() {
+        actionMessage.clearMessages();
+        controlProperty.setErrorMessage(null);
+        if( isRequired() && ValueUtil.isEmpty(getText()) ) {
+            actionMessage.addMessage("", "{0} is required", new Object[] {getText()});
+            controlProperty.setErrorMessage(actionMessage.toString());
+        }
+    }
+
+    public ActionMessage getActionMessage() {
+        return actionMessage;
+    }
+
+    public ControlProperty getControlProperty() {
+        return controlProperty;
+    }
+    
+    
+    //<editor-fold defaultstate="collapsed" desc="  DateFieldSupport (class)  ">
+    private class DateFieldSupport implements FocusListener {
+        
+        public void focusGained(FocusEvent e) {
+            showFormattedValue(false);
+        }
+        
+        public void focusLost(FocusEvent e) {
+            showFormattedValue(true);
+        }
+        
+    }
+    //</editor-fold>
+    
+}
---
+++ client/rameses-client-ui/src/com/rameses/rcp/control/XPasswordField.java
@@ -0,0 +1,235 @@
+package com.rameses.rcp.control;
+
+import com.rameses.rcp.framework.Binding;
+import com.rameses.rcp.framework.ClientContext;
+import com.rameses.rcp.ui.Containable;
+import com.rameses.rcp.ui.ControlProperty;
+import com.rameses.rcp.ui.UIControl;
+import com.rameses.rcp.ui.UIInput;
+import com.rameses.rcp.ui.Validatable;
+import com.rameses.rcp.util.ActionMessage;
+import com.rameses.rcp.util.UIControlUtil;
+import com.rameses.rcp.util.UIInputUtil;
+import com.rameses.util.ValueUtil;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.Beans;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.ImageIcon;
+import javax.swing.JPasswordField;
+import javax.swing.text.DefaultCaret;
+
+/**
+ *
+ * @author Windhel
+ */
+
+public class XPasswordField extends JPasswordField implements UIInput, Validatable, Containable {
+    
+    private static Font FONT = new Font("Dialog", Font.PLAIN, 24);
+    
+    private ImageIcon[] icons;
+    private String[] iconPathList;
+    private int imgHeight;
+    private int charWidth;
+    private int iconIndex;
+    private int psswrdWidth;
+    private DefaultCaret caret;
+    private List iconIndexList = new ArrayList();
+    private boolean nullWhenEmpty = true;
+    private String[] depends;
+    private Binding binding;
+    private int index;
+    private char passwordChar = '*';
+    private ActionMessage actionMessage = new ActionMessage();
+    private ControlProperty controlProperty = new ControlProperty();
+    
+    public XPasswordField() {}
+    
+    public Object getValue() {
+        if( Beans.isDesignTime())
+            return "";
+        
+        return String.valueOf(getPassword());
+    }
+    
+    public void setValue(Object value) {
+        setText( value==null? "" : value.toString() );
+    }
+    
+    public boolean isNullWhenEmpty() {
+        return nullWhenEmpty;
+    }
+    
+    public void setNullWhenEmpty(boolean nullWhenEmpty) {
+        this.nullWhenEmpty = nullWhenEmpty;
+    }
+    
+    public String[] getDepends() {
+        return depends;
+    }
+    
+    public void setDepends(String[] depends) {
+        this.depends = depends;
+    }
+    
+    public int getIndex() {
+        return index;
+    }
+    
+    public void setIndex(int index) {
+        this.index = index;
+    }
+    
+    public Binding getBinding() {
+        return binding;
+    }
+    
+    public void setBinding(Binding binding) {
+        this.binding = binding;
+    }
+    
+    public void refresh() {
+        Object value = UIControlUtil.getBeanValue(this);
+        setValue(value);
+    }
+    
+    public void load() {
+        setInputVerifier(UIInputUtil.VERIFIER);
+        
+        if( getEchoChar() == ' ') {
+            setFont(FONT);
+            addKeyListener(new KeyListenerSupport());
+            charWidth = getFontMetrics(FONT).charWidth(passwordChar);
+            setMargin(new Insets(0,0,0,0));
+            psswrdWidth= ((int)getSize().getWidth() / charWidth) - 1;
+        }
+    }
+    
+    public int compareTo(Object o) {
+        if(o == null || !(o instanceof UIControl) )
+            return 0;
+        
+        UIControl u = (UIControl)o;
+        return this.index - u.getIndex();
+    }
+    
+    public void setEchoChar(char c) {
+        this.passwordChar = c;            
+        icons = null;
+        iconPathList = null;
+    }
+    
+    public char getEchoChar() {
+        return passwordChar;
+    }
+    
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if(Beans.isDesignTime()) return;
+        
+        if( getEchoChar() == ' ') {
+            int x = 0;
+            int iconIndex = 0;
+            if( getPassword().length >= psswrdWidth)
+                x = getPassword().length - psswrdWidth + 1;
+            else
+                x = 1;
+            for(int counter = x, passLength = 0; counter < getPassword().length + 1 ; counter++) {
+                if(! (passLength == (psswrdWidth - 1)))
+                    passLength++;
+                if(iconIndexList.isEmpty() == true)
+                    iconIndex = 0;
+                else
+                    iconIndex = Integer.parseInt( (String) iconIndexList.get(counter - 1) );
+                g.drawImage( icons[iconIndex].getImage(), passLength * charWidth, (getHeight() - imgHeight) / 2 , charWidth, imgHeight, null);
+            }
+        }
+    }
+    
+    public void setMargin(Insets m) {
+        Insets insets = new Insets(m.top, charWidth, m.bottom, charWidth);
+        super.setMargin(insets);
+    }
+    
+    public String[] getIcons() {
+        return iconPathList;
+    }
+    
+    public void setIcons(String[] iconPathList) {
+        passwordChar = ' ';
+        this.iconPathList = iconPathList;        
+        if(Beans.isDesignTime()) return;
+        
+        icons = new ImageIcon[iconPathList.length];
+        for(int i = 0; i < icons.length; i++) {
+            URL res = getImageResource(iconPathList[i]);
+            if( res != null )
+                icons[i] = new ImageIcon(res);
+        }
+        imgHeight = icons[0].getIconHeight();
+    }
+    
+    private URL getImageResource(String path) {
+        ClassLoader cl = ClientContext.getCurrentContext().getClassLoader();
+        return cl.getResource(path);
+    }
+
+    public String getCaption() {
+        return controlProperty.getCaption();
+    }
+
+    public void setCaption(String caption) {
+        controlProperty.setCaption(caption);
+    }
+
+    public boolean isRequired() {
+        return controlProperty.isRequired();
+    }
+
+    public void setRequired(boolean required) {
+        controlProperty.setRequired(required);
+    }
+
+    public void validateInput() {
+        actionMessage.clearMessages();
+        controlProperty.setErrorMessage(null);
+        if( isRequired() && getPassword().length<=0 ) {
+            actionMessage.addMessage("", "{0} is required.", new Object[] {getCaption()});
+            controlProperty.setErrorMessage(actionMessage.toString());
+        }
+    }
+
+    public ActionMessage getActionMessage() {
+        return actionMessage;
+    }
+
+    public ControlProperty getControlProperty() {
+        return controlProperty;
+    }
+    
+    
+    //<editor-fold defaultstate="collapsed" desc="  KeyListenerSupport (class)  ">
+    private class KeyListenerSupport implements KeyListener{
+        public void keyTyped(KeyEvent e) {
+        }
+        
+        public void keyPressed(KeyEvent e) {
+            int idx = 0;
+            // 35[Home] 36[End] 37-40[directional keys]
+            if( !( e.getKeyCode() >= 35 && e.getKeyCode() <= 40 ) ){
+                idx = getPassword().length % icons.length;
+                iconIndexList.add( getCaretPosition(),"" +  idx);
+            }
+        }
+        
+        public void keyReleased(KeyEvent e) {
+        }
+    }
+    //</editor-fold>
+}
