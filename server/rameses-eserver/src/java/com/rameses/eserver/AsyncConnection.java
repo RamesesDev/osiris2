35ff57242be37a435a5196a6cc55e04711fc17d3
Jayrome Vergara <jaycverg@gmail.com>
2010-07-24 15:54:44 +0800

    added AsyncConnection and AsyncConnectionMBean on rameses-eserver

---
+++ server/rameses-eserver/src/java/com/rameses/eserver/AsyncConnection.java
@@ -0,0 +1,116 @@
+package com.rameses.eserver;
+
+import com.rameses.eserver.JndiUtil;
+import com.rameses.messaging.ConnectionManager;
+import com.rameses.messaging.MessageListener;
+import com.rameses.messaging.MessagingConnection;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import javax.naming.InitialContext;
+
+/*
+ * AsyncConnection.java
+ *
+ * Created on July 24, 2010, 1:35 PM
+ * @author jaycverg
+ */
+
+public class AsyncConnection implements AsyncConnectionMBean, Serializable, MessageListener {
+    
+    private String jndiName;
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+    private String driverClass;
+    
+    private MessagingConnection con;
+    
+    public AsyncConnection() {
+    }
+    
+    public void start() throws Exception {
+        System.out.println("binding ConnectionTest: " + jndiName);
+        
+        InitialContext ctx = new InitialContext();        
+        JndiUtil.bind(ctx, jndiName, this);
+
+        con = ConnectionManager.getInstance().getConnection(driverClass, host, username, password);
+        con.addListener(this);        
+        con.open();
+    }
+    
+    public void stop() throws Exception {
+        InitialContext ctx = new InitialContext();
+        JndiUtil.unbind(ctx, jndiName);
+        
+        con.close();
+    }
+    
+    public Map getConf() {
+        Map m = new HashMap();
+        m.put("host", host);
+        m.put("port", port);
+        m.put("driverClass", driverClass);
+        
+        return m;
+    }
+    
+    public MessagingConnection getConnection() throws Exception {
+        return con;
+    }
+
+    public void onMessage(Object message) {
+        System.out.println("receiving message....." + message);
+    }
+
+    public String getJndiName() {
+        return jndiName;
+    }
+
+    public void setJndiName(String jndiName) {
+        this.jndiName = jndiName;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDriverClass() {
+        return driverClass;
+    }
+
+    public void setDriverClass(String driverClass) {
+        this.driverClass = driverClass;
+    }
+    
+}
