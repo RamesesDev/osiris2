2c307497b1f4bcb491e03b4454cbf59451b2d027
Jayrome Vergara <jaycverg@gmail.com>
2010-07-31 11:45:24 +0800

    separated graph.jar:  moved chart tag to common-web.jar  moved models to rameses-chart.jar
    upgraded jquery version to 1.4.7

--- tests/test-client/src/test/client/HomePage.java
+++ tests/test-client/src/test/client/HomePage.java
@@ -15,33 +15,29 @@ public class HomePage extends javax.swing.JPanel {
     
     // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
     private void initComponents() {
-        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         setPreferredSize(new java.awt.Dimension(513, 261));
-        jLabel1.setFont(new java.awt.Font("Dialog", 1, 18));
-        jLabel1.setText("Welcome to Rameses Platform Suite");
+        jLabel2.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/loading.gif")));
+        jLabel2.setOpaque(true);
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(layout.createSequentialGroup()
-                .addContainerGap()
-                .add(jLabel1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 489, Short.MAX_VALUE)
-                .addContainerGap())
+            .add(jLabel2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 513, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(layout.createSequentialGroup()
-                .addContainerGap()
-                .add(jLabel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 80, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(169, Short.MAX_VALUE))
+            .add(jLabel2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 261, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
     
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
     
 }
---
+++ web/commonweb/src/com/rameses/web/component/chart/ChartPhaseListener.java
@@ -0,0 +1,81 @@
+
+package com.rameses.web.component.chart;
+
+import com.rameses.chart.AbstractChartHandler;
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+import java.io.OutputStream;
+import javax.faces.context.FacesContext;
+import java.awt.image.BufferedImage;
+import java.util.Map;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ChartPhaseListener implements PhaseListener {
+    
+    public static final String FILE_ID = "graph_fileID";
+    public static final String ABORT_PHASE4 = "_ABORT_PHASE4";
+    
+    public ChartPhaseListener() {
+    }
+    
+    public void beforePhase(PhaseEvent phaseEvent) {
+        if (phaseEvent.getPhaseId() != PhaseId.RESTORE_VIEW) return;
+        
+        FacesContext context = phaseEvent.getFacesContext();
+        if (context.getResponseComplete()) return;
+        
+        HttpServletRequest req = (HttpServletRequest) context.getExternalContext().getRequest();
+        
+        if (req.getParameter(FILE_ID)!=null) {
+            String graphId = req.getParameter(FILE_ID);
+            
+            Map session = context.getExternalContext().getSessionMap();
+            AbstractChartHandler h = (AbstractChartHandler) session.get(graphId);
+
+            if (h != null) {
+                context.responseComplete();
+                HttpServletResponse res = (HttpServletResponse)context.getExternalContext().getResponse();
+                OutputStream out = null;
+                BufferedImage image = null;
+                try {
+                    out = res.getOutputStream();
+                    int width = Integer.parseInt(h.getWidth());
+                    int height = Integer.parseInt(h.getHeight());
+                    image =  h.createChart().createBufferedImage(width, height);
+                    JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
+                    encoder.encode(image);
+                    out.flush();
+                } catch(NumberFormatException ex) {
+                    throw new IllegalStateException("[height and width value] for chart must be of integer type.");
+                } catch(Exception ex) {
+                    throw new IllegalStateException(ex);
+                } finally {
+                    try { out.close(); } catch(Exception ign){;}
+                    image = null;
+                    session.remove(graphId);
+                }
+            }
+        }
+    }
+    
+    public void afterPhase(PhaseEvent phaseEvent) {
+        /**
+         * used when refreshing the chart using ajax
+         * - abort phase 4 after the chart component had put the 
+         *   chart handler back to the session
+         */
+        if (phaseEvent.getPhaseId().getOrdinal() != 4) return;
+        FacesContext context = phaseEvent.getFacesContext();
+        HttpServletRequest req = (HttpServletRequest) context.getExternalContext().getRequest();
+        if (req.getAttribute(ABORT_PHASE4) == null) return;
+        context.responseComplete();
+    }
+    
+    public PhaseId getPhaseId() {
+        return PhaseId.ANY_PHASE;
+    }
+}
