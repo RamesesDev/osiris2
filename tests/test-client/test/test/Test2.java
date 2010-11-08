978bb1d154428ad08795286ccdb36f154d31a6cb
unknown <rameses@.(none)>
2010-11-08 16:25:31 +0800

    map out Action properties to XButton's properties

---
+++ tests/test-client/test/test/Test2.java
@@ -0,0 +1,28 @@
+/*
+ * Test2.java
+ * JUnit based test
+ *
+ * Created on November 8, 2010, 2:38 PM
+ */
+
+package test;
+
+import com.rameses.osiris2.Folder;
+import com.rameses.osiris2.client.OsirisContext;
+import junit.framework.*;
+
+/**
+ *
+ * @author rameses
+ */
+public class Test2 extends TestCase {
+    
+    public Test2(String testName) {
+        super(testName);
+    }
+    
+    public void testHello() {
+
+    }
+
+}
