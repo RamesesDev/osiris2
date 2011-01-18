d33adfc78b1e0222216e225a26cd2612e2addfc4
elmo <ms@.(none)>
2011-01-18 14:00:54 +0800

    updated eserver for messaging

---
+++ server/async-impl/src/com/rameses/eserver/async/RemoteScriptMDB.java
@@ -0,0 +1,22 @@
+/*
+ * ScriptMDB.java
+ *
+ * Created on October 20, 2010, 8:00 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.rameses.eserver.async;
+
+import com.rameses.eserver.AbstractScriptMDB;
+import javax.jms.MessageListener;
+
+
+public class RemoteScriptMDB extends AbstractScriptMDB implements MessageListener {
+    
+    public boolean isRemote() {
+        return true;
+    }
+    
+}
